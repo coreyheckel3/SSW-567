2f9911c63ebfe193e2e1cfad2e03ca2631f14195
coreyheckel3 <coreyheckel3@gmail.com>
2023-09-09 12:32:57 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import datetime
+
+currentTime = datetime.datetime.now()
+def my_brand(assignment):
+    print("=*=*=*= Corey Heckel =*=*=*=")
+    print("=*=*=*= Course 2023S-SSW567-WS =*=*=*=")
+    print("=*=*=*= " + assignment + " =*=*=*=")
+    print("=*=*=*= Current Time: " + currentTime.strftime("%Y-%m-%d %H:%M:%S") + " =*=*=*=")
+
+my_brand("HW 00 - Tools Setup")
+print("Hello World")
+my_brand("HW 00 - Tools Setup")
